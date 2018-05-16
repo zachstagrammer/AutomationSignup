08948a6e28d9f20b95a3eb9a5b14dac37a898f55
Zach Phillips <zjohnny94@gmail.com>
2018-05-15 23:42:23 -0500

    Initial setup

---
+++ AutomationSignup/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationSignup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://developers.google.com/";
+
+
+        }
+    }
+}
