88f2e8544d956e805461bc08d760c1acfd278361
Zach Phillips <zjohnny94@gmail.com>
2018-05-21 19:15:24 -0500

    Confirmed h3 tag and added error exceptions

--- AutomationSignup/Program.cs
+++ AutomationSignup/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace AutomationSignup
 {
@@ -12,9 +13,9 @@ namespace AutomationSignup
     {
         static void Main(string[] args)
         {
-            //Launch google developers webpage
-
             IWebDriver driver = new ChromeDriver();
+
+            //Launch google developers webpage
 
             driver.Url = "https://developers.google.com/";
             Console.WriteLine("Opened Chrome Browser to https://developers.google.com/...");
@@ -25,25 +26,52 @@ namespace AutomationSignup
             string googleCloudNextDescr = "Hone your skills on the latest cloud technologies with Google experts at hundreds of breakout sessions and interactive on-demand hands-on labs and bootcamps. You'll have the opportunity to engage with the best minds in cloud technology on how your industry is adapting, innovating, and growing with cloud.";
             string getEventDescrText = driver.FindElement(By.ClassName("devsite-landing-row-item-description-content")).Text;
 
-            if (getEventDescrText.Contains(googleCloudNextDescr))
+            try
             {
-                Console.WriteLine("Google Cloud Next '18 event description confirmed...");
+                if (getEventDescrText.Contains(googleCloudNextDescr))
+                {
+                    Console.WriteLine("Google Cloud Next '18 event description confirmed...");
+                }
+                else
+                {
+                    Console.WriteLine("Could not find description for Google Cloud Next '18.  Closing browser.");
+                    driver.Close();
+                }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Could not find description for Google Cloud Next '18.  Closing browser.");
-                driver.Close();
+                Console.WriteLine(e.Message);
             }
 
 
             // Click on the Google Cloud Next '18 event
 
             driver.FindElement(By.Id("google-cloud-next-18br-july-24-26-san-francisco-usa")).Click();
-            Console.WriteLine("Navigated to the Google Cloud Next '18 event...");
+            Console.WriteLine("Navigated to the Google Cloud Next '18 event page...");
 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30); ;
 
             // Confirm that the first H3 on the page is "Imagine"
 
+            try
+            {
+                var h3Tag = driver.FindElement(By.ClassName("title")).Text;
+                
+                if (h3Tag == "Imagine")
+                {
+                    Console.WriteLine("Confirmed H3 tag \"Imagine\"...");
+                }
+                else
+                {
+                    Console.WriteLine("Could not find H3 tag \"Imagine\"");
+                    driver.Close();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
 
         }
